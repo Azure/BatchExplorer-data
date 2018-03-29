53a38fb98a19f998f9a901f73c2e95817ba4b7b2
tamishra@microsoft.com <tamishra@microsoft.com>
2018-03-29 14:12:26 +1300

    blobfuse sasToken has issues, Temporary script changes to support Keys.

--- ncj/vray/render-linux/installblobfuse.sh
+++ ncj/vray/render-linux/installblobfuse.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 RC_PATH="$AZ_BATCH_JOB_PREP_WORKING_DIR/connection.cfg"
-# INPUT_FILEGROUP_NAME="$1"
-# INPUT_FILEGROUP_SAS="$2"
 # Get the account name from the SAS URL
 theAccountName="$(echo $INPUT_FILEGROUP_SAS | cut -d'/' -f3 | cut -d'.' -f1)"
 sudo echo "accountName $theAccountName" > $RC_PATH
-sudo echo "sasToken $INPUT_FILEGROUP_SAS" >> $RC_PATH
+sudo echo "accountKey $INPUT_FILEGROUP_KEY" >> $RC_PATH
 sudo echo "containerName $INPUT_FILEGROUP_NAME" >> $RC_PATH
 # Configure the apt repository for Microsoft products following: 
 # Install repository configuration
@@ -18,7 +16,7 @@ sudo rpm --import ./microsoft.asc
 sudo yum install blobfuse fuse -y
 # Configuring and Running.
 sudo mkdir $AZ_BATCH_JOB_PREP_WORKING_DIR/blobfusetmp
-# This is temporary naming for testing
+# This is folder naming with the filegroup name.
 sudo mkdir $AZ_BATCH_JOB_PREP_WORKING_DIR/$INPUT_FILEGROUP_NAME
 # Below is the config file approach
 blobfuse $AZ_BATCH_JOB_PREP_WORKING_DIR/$INPUT_FILEGROUP_NAME --tmp-path=/mnt/blobfusetmp -o attr_timeout=240 -o entry_timeout=240 -o negative_timeout=120 --config-file="$RC_PATH"
