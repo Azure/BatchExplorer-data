6ab20b838c40fc565c531fb69feff46714428313
Christian Smith <christis@microsoft.com>
2018-04-11 10:31:04 +0100

    Print 3ds max args.

--- ncj/3dsmax/scripts/3dsmax.ps1
+++ ncj/3dsmax/scripts/3dsmax.ps1
@@ -212,6 +212,8 @@ if ($env:3DSMAX_2018 -and (Test-Path "$env:3DSMAX_2018"))
     $max_exec = "${env:3DSMAX_2018}\3dsmaxcmdio.exe"
 }
 
+Write-Host "Executing $max_exec -secure off -v:5 -rfw:0 $cameraParam $renderPresetFileParam $additionalArgumentsParam -preRenderScript:"$pre_render_script" -start:$start -end:$end -outputName:"$outputName" -width:$width -height:$height $pathFileParam "$sceneFile""
+
 cmd.exe /c $max_exec -secure off -v:5 -rfw:0 $cameraParam $renderPresetFileParam $additionalArgumentsParam -preRenderScript:"$pre_render_script" -start:$start -end:$end -outputName:"$outputName" -width:$width -height:$height $pathFileParam "$sceneFile" `>Max_frame.log 2`>`&1
 $result = $lastexitcode
 
