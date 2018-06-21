2f74b19f84c7bee07106a7a81ea28f77477d038f
Andrew Scobie <ascobie@microsoft.com>
2018-06-21 14:59:51 +1200

    keep unix line endings

--- ncj/blender/scripts/setup-linux-pool.sh
+++ ncj/blender/scripts/setup-linux-pool.sh
@@ -4,7 +4,13 @@ apt-get install -y imagemagick
 apt-get install -y blender
 apt-get install -y python-pip
 pip install azure-batch
+wget -q https://packages.microsoft.com/config/ubuntu/16.04/packages-microsoft-prod.deb
+sudo dpkg -i packages-microsoft-prod.deb
+sudo apt-get install apt-transport-https
+sudo apt-get update
+sudo apt-get install dotnet-sdk-2.1
 wget -O azcopy.tar.gz https://aka.ms/downloadazcopyprlinux
 tar -xf azcopy.tar.gz
 ./install.sh
 exit $?
+
