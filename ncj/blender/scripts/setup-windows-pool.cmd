9f028217911c78a328a5be03afc03de4e5e1f6cf
Andrew Scobie <ascobie@microsoft.com>
2018-07-18 14:11:29 +1200

    fix script

--- ncj/blender/scripts/setup-windows-pool.cmd
+++ ncj/blender/scripts/setup-windows-pool.cmd
@@ -1,9 +1,7 @@
 
 echo # installing choco ...
 choco install python -y 
-refreshenv
 echo # installing azure-batch sdk for python ...
 pip install azure-batch
-refreshenv 
 echo Exit Code is %errorlevel%
 exit /b %errorlevel%
