1b02fbf96c682ba3551194c8b201651e341eaa0c
Christian Smith <christis@microsoft.com>
2018-09-19 21:15:44 +1000

    Revert usage of env vars for the max paths.

--- ncj/3dsmax/scripts/PrepareVrayServices.bat
+++ ncj/3dsmax/scripts/PrepareVrayServices.bat
@@ -1,14 +1,13 @@
-@echo off
 
 set MAX_VERSION=%1
 set VRAY_RENDERER=%2
 set VRAY_PORT=%3
 
 rem We need to create the config files first
-echo.[Directories] > %3DSMAX_2018%vrayspawner.ini
-echo.AppName=%3DSMAX_2018%3dsmaxio.exe >> %3DSMAX_2018%vrayspawner.ini
-echo.[Directories] > %3DSMAX_2019%vrayspawner.ini
-echo.AppName=%3DSMAX_2019%3dsmaxio.exe >> %3DSMAX_2019%vrayspawner.ini
+echo.[Directories] > C:\Autodesk\3dsMax2018\vrayspawner.ini
+echo.AppName=C:\Autodesk\3dsMax2018\3dsmaxio.exe >> C:\Autodesk\3dsMax2018\vrayspawner.ini
+echo.[Directories] > C:\Autodesk\3dsMax2019\vrayspawner.ini
+echo.AppName=C:\Autodesk\3dsMax2019\3dsmaxio.exe >> C:\Autodesk\3dsMax2019\vrayspawner.ini
 
 IF %VRAY_RENDERER%=="VRayAdv" (
     IF %MAX_VERSION%=="2018" ( 
